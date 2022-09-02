f86646398b7ca34a749b6570ccdd76bbe3bbb96c
Alex Rivie <rivie.alex@gmail.com>
2022-09-02 06:34:26 -0400

    add numberLookup and move update() to end of hex.castLine()

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ const HEX_MAX = 6
 const hexagram = {
   lines: [],
   number: 0,
+  article: null,
   isFull: function() {
     if (this.lines.length >= HEX_MAX) return true
     else false
   },
   isChanging: function() {
-    if (this.lines.contains(6) || this.lines.contains(9)) return true
+    if (this.lines.includes(6) || this.lines.includes(9)) return true
     return false
   },
-  castLine: function() {
+  castLine: async function() {
     this.lines.push(PROBABILITY_ARR[Math.floor(Math.random()*PROBABILITY_ARR.length)])
+    if (this.isFull()) {
+      this.number = await getNumber(this)     
+    }
+    this.article = hexObjToArticle(this)
+    update()
   },
   calculateBase: function() {
-    this.lines.map(x => {
+    return this.lines.map(x => {
       if (x === 6) return 8
       if (x === 9) return 7
       return x
@@ -30,10 +36,16 @@ const hexagram = {
       if (x === 9) return 6
       return x
     })
-  },
-  compareLines: function(hexObj) {
-    if (JSON.stringify(this.lines) === JSON.stringify(hexObj.lines)) return true
-    else return false
+  }
+}
+
+function hexLineToCssClass(line) {
+  switch(line) {
+    case 0: return 'yin placeholder'
+    case 6: return 'yin old'
+    case 7: return 'yang'
+    case 8: return 'yin' 
+    case 9: return 'yang old'
   }
 }
 
@@ -48,52 +60,45 @@ function hexObjToArticle(hexObj) {
   article.appendChild(ul)
   if (hexObj.isFull()) {
     const p = document.createElement('p')
-    p.innerHTML = getNumber(hexObj)
-    console.log(p.innerHTML)
+    p.innerHTML = hexObj.number
     article.appendChild(p)
   }
   return article
 }
 
-function hexLineToCssClass(line) {
-  switch(line) {
-    case 0: return 'yin placeholder'
-    case 6: return 'yin old'
-    case 7: return 'yang'
-    case 8: return 'yin' 
-    case 9: return 'yang old'
+async function fetchHexagramArray() {
+  return fetch('hexArr.json')
+    .then(result => result.json())
+    .then(data => {
+        return data
+    })
+}
+
+async function getNumber(hexObj) {
+  let hexagrams = await fetchHexagramArray()
+  for (const hexagram of hexagrams) {
+    if (sameArray(hexObj.calculateBase(), hexagram.lines)) {
+      return hexagram.number
+    }
   }
 }
 
-async function fetchHexagramsJSON() {
-  const response = await fetch('./hexArr.json')
-  const hexagrams = await response.json()
-  return hexagrams
-}
-
-function getNumber(hexObj) {
-  fetchHexagramsJSON().then(hexagrams => {
-    for (const hexagram of hexagrams) {
-      if (hexObj.compareLines(hexagram)) {
-        console.log(hexagram.number)
-      }
-    }
-  })
-}
-
-function update() {
+async function update() {
   const section = document.querySelector('section')
   section.removeChild(document.querySelector('section article'))
-  const article = hexObjToArticle(hex)
-  section.appendChild(article)
-
-  if (hex.isFull()) {
+  section.appendChild(hex.article)
+  if (hex.isFull() && hex.isChanging()) {
     secondary.lines = hex.calculateChanging()
-    if (!hex.compareLines(secondary)) {
-      section.appendChild(hexObjToArticle(secondary))
-    }
+    secondary.number = await getNumber(secondary)
+    secondary.article = hexObjToArticle(secondary)
+    section.appendChild(secondary.article)
   }
 }
 
 const hex = hexagram
 const secondary = Object.assign({}, hexagram)
+
+function sameArray(x, y) {
+  if (JSON.stringify(x) === JSON.stringify(y)) return true
+  else false
+}
